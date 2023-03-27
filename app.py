9a09d1b6d6ead7c2d1eb570d69d3c28e111068c0
AyadiUSMA <jeanpierre1934@gmail.com>
2023-03-27 11:13:52 +0000

    update the app file as v0

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask,Response
 from flask_restful import Resource, Api, reqparse
 from datetime import datetime, timedelta
+import time
 import logging
 import requests
 import psycopg2
+import nest_asyncio
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from requests_html import HTMLSession,AsyncHTMLSession
 from benddao_func import check_db_auctions,get_data_from_url_element,insert_df_database,send_new_auct_email
 
 app = Flask(__name__)
-
+app.config['TIMEOUT'] = 300
 
 @app.route('/')
 def hello():
@@ -15,8 +25,14 @@ def hello():
 
 @app.route('/verif_auctions')
 def verif_auctions():
+    def generate():
+        yield scrape()
+    return Response(generate(),mimetype='text/plain')
+
+
+
+def scrape():
     session = HTMLSession()
-    nest_asyncio.apply()
     url = "https://www.benddao.xyz/en/auctions/loans-in-auction/"
     website = "https://www.benddao.xyz/en/auctions/loans-in-auction/"
 
@@ -25,8 +41,8 @@ def verif_auctions():
     driver = webdriver.Chrome(options=options)
 
     driver.get(url)
-    time.sleep(15)
-    wait = WebDriverWait(driver, 20)
+    time.sleep(10)
+    wait = WebDriverWait(driver, 10)
     element = wait.until(EC.presence_of_element_located((By.ID,'__next')))
     #   driver.implicitly_wait(5)
     soup = BeautifulSoup(driver.page_source,'html.parser')
@@ -36,6 +52,8 @@ def verif_auctions():
         f.write(pretty_html)
     f.close()
     driver.quit()
-    return "ok"
+    
+    
+    return pretty_html
 
 
